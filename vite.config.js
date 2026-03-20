6fb3fa907df3e80a58265b2af750d09d60b1ffac
escuela-sabatica <sauljoses@gmail.com>
2026-03-19 23:06:49 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
    export default defineConfig({
-     plugins: [vue()]
+     plugins: [vue()],
+      base: '/leccion12/'
    })
